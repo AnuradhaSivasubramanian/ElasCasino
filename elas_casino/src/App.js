0d64262c3e079fefb15bd0dc8e6362a26eaf1d93
Lígia Souza <ligia.o.souza@gmail.com>
2020-02-17 12:14:26 +0100

    2 version 17 feb

--- elas_casino/src/App.js
+++ elas_casino/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Ligia from "./component/Ligia";
+import Ligia from "./component/GameLogic";
 import Winner from "./component/Winner"
 import Loser from "./component/Loser"
+import GameTable from "./component/GameTable"
 class App extends React.Component {
   render() {
     return (
       <main>
-        <Ligia/>
+        <GameTable/>
       </main>
     );
   }
---
+++ elas_casino/src/component/GameTable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function GameTable(){
+return(
+<div>
+    
+</div>
+)
+}
+
+export default GameTable
