97ae9d7db2d8193a58a5d774c7c796ae08ed6394
Lígia Souza <ligia.o.souza@gmail.com>
2020-02-18 20:38:46 +0100

    comiting few changins in the game table and start of turn your phone

--- elas_casino/src/App.js
+++ elas_casino/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 /* import Ligia from "./component/GameLogic";
 import Winner from "./component/Winner"
-import Loser from "./component/Loser" */
-import GameTable from "./component/GameTable"
+import Loser from "./component/Loser" 
+import GameTable from "./component/GameTable"*/
+import Home from "./component/Home"
+/* import TurnYourPhone from './component/TurnYourPhone'
+ */
 class App extends React.Component {
   render() {
     return (
       <main>
-        <GameTable/>
+{/*       <TurnYourPhone className="turn_your_phone"/>
+ */}
+        <Home/>
+
       </main>
     );
   }
--- elas_casino/src/component/Home.js
+++ elas_casino/src/component/Home.js
@@ -53,14 +53,3 @@ class Home extends React.Component {
 }
 export default Home;
 
-import Home from "./component/Home";
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <Home />
-      </main>
-    );
-  }
-}
-export default App;
