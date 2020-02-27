003b73b83ad89204df252d7e980597ab48a82d32
anu radha <anu.sivasubramanian@gmail.com>
2020-02-27 10:11:53 +0100

    added card into backofdecks - using image file now

--- elas_casino/src/component/BackOfCards.js
+++ elas_casino/src/component/BackOfCards.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import "./scss/BackOfTheDeck.scss"
+import React from "react";
+import "./scss/BackOfTheDeck.scss";
+import Card from "./Photos/BackOfaDeck2.png";
 
-function BackOfCards({flagValue,backofthedeckID}){
-
-    return(
-        <div>
-        <img
-        src="https://cdn.pixabay.com/photo/2012/05/07/18/53/card-game-48982_640.png"
+function BackOfCards({ flagValue, backofthedeckID }) {
+  return (
+    <div>
+      <img
+        src={Card}
         alt="Back of the deck of cards"
-        className={flagValue===1 || flagValue===2?"backofadeck_on" : "backofadeck_off"}
-        id= {backofthedeckID }       />
-        </div>
-    )
+        className={
+          flagValue === 1 || flagValue === 2
+            ? "backofadeck_on"
+            : "backofadeck_off"
+        }
+        id={backofthedeckID}
+      />
+    </div>
+  );
 }
 
-export default BackOfCards
+export default BackOfCards;
